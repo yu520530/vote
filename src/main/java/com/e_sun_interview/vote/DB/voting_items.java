8d1b4a73b57b3d44538790fc524edccedcfeaed5
yo <ya90312@gmail.com>
2024-08-31 04:51:40 +0800

    feat: 新增投票項目資料表之欄位

--- src/main/java/com/e_sun_interview/vote/DB/voting_items.java
+++ src/main/java/com/e_sun_interview/vote/DB/voting_items.java
@@ -1,4 +1,20 @@
 package com.e_sun_interview.vote.DB;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+// voting_items紀錄所有投票項目之名稱、ID
+@Getter
+@Setter
+@Entity
+@Table
 public class voting_items {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String name;
+
 }
