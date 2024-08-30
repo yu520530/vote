bb017beb71876056a7bf5de3a7903316472c34c7
yo <ya90312@gmail.com>
2024-08-31 04:45:43 +0800

    feat: 新增使用者資料表之欄位

--- src/main/java/com/e_sun_interview/vote/DB/users.java
+++ src/main/java/com/e_sun_interview/vote/DB/users.java
@@ -1,4 +1,20 @@
 package com.e_sun_interview.vote.DB;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+// users紀錄所有使用者之姓名、ID
+@Getter
+@Setter
+@Entity
+@Table
 public class users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String username;
+
 }
