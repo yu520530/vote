26ac40eb955887c754943e4d444888e20d567fc4
yo <ya90312@gmail.com>
2024-08-30 17:47:09 +0800

    feat: initial

---
+++ src/main/java/com/e_sun_interview/vote/VoteApplication.java
@@ -0,0 +1,13 @@
+package com.e_sun_interview.vote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoteApplication.class, args);
+	}
+
+}
