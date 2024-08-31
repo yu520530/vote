90b7a6bbe94d21d1cedbbbf38166ac945f0aa52f
yo <ya90312@gmail.com>
2024-08-31 16:40:10 +0800

    feat: controller新增路由

--- src/main/java/com/e_sun_interview/vote/controller/CMSController.java
+++ src/main/java/com/e_sun_interview/vote/controller/CMSController.java
@@ -1,13 +1,29 @@
 package com.e_sun_interview.vote.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.e_sun_interview.vote.DB.Voting_items;
+import com.e_sun_interview.vote.service.CMSService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
 
 @RestController
+@RequestMapping("/cms")
 public class CMSController {
-    @RequestMapping("/cms")
+
+    @Autowired
+    CMSService cmsService;
+
+    @GetMapping("/list")
     // 這個地方需要顯示後台資料
-    public String hello() {
-        return "Hello World!";
+    public List<Voting_items> listVotingItem() {
+        return cmsService.getVotingItem();
     }
+
+    @PatchMapping("/create")
+    public String createVotingItem(@RequestBody Voting_items item){
+        cmsService.createVotingItem(item);
+        return "Success";
+    }
+
 }
