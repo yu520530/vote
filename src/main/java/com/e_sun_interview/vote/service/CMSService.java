a166df3ad7e6ec709b17999fc7ec4dd30f1b667d
yo <ya90312@gmail.com>
2024-08-31 16:39:50 +0800

    feat: service 新增 新增以及取得voting_item之兩種方法

--- src/main/java/com/e_sun_interview/vote/service/CMSService.java
+++ src/main/java/com/e_sun_interview/vote/service/CMSService.java
@@ -1,7 +1,22 @@
 package com.e_sun_interview.vote.service;
 
+import com.e_sun_interview.vote.DB.Voting_items;
+import com.e_sun_interview.vote.repository.CMSRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 @Service
 public class CMSService {
+
+    @Autowired
+    CMSRepository cmsRepository;
+
+    public List<Voting_items> getVotingItem() {
+        return cmsRepository.findAll();
+    }
+
+    public void createVotingItem(Voting_items item){
+        cmsRepository.save(item);
+    }
 }
