fdc9c7716dac2d6bc51d817cde1fc6f398771464
yo <ya90312@gmail.com>
2024-08-31 20:25:10 +0800

    feat: 改回Integer

--- src/main/java/com/e_sun_interview/vote/repository/CMSRepository.java
+++ src/main/java/com/e_sun_interview/vote/repository/CMSRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CMSRepository extends JpaRepository<Voting_items, Long> {
+public interface CMSRepository extends JpaRepository<Voting_items, Integer> {
 }
