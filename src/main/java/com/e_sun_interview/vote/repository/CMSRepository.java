a0d2428c26ccb346e55c843f0a5404f1d8d8fd04
yo <ya90312@gmail.com>
2024-08-31 15:16:32 +0800

    feat: extends JpaRepository

--- src/main/java/com/e_sun_interview/vote/repository/CMSRepository.java
+++ src/main/java/com/e_sun_interview/vote/repository/CMSRepository.java
@@ -1,7 +1,9 @@
 package com.e_sun_interview.vote.repository;
 
+import com.e_sun_interview.vote.DB.Voting_items;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CMSRepository {
+public interface CMSRepository extends JpaRepository<Voting_items, Integer> {
 }
