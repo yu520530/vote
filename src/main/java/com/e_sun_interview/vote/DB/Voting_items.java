781765c72bdf7ab9cc81e110656d57a9e98df385
yo <ya90312@gmail.com>
2024-09-01 19:34:00 +0800

    feat: 建立一對多關聯

--- src/main/java/com/e_sun_interview/vote/DB/Voting_items.java
+++ src/main/java/com/e_sun_interview/vote/DB/Voting_items.java
@@ -1,8 +1,14 @@
 package com.e_sun_interview.vote.DB;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 // voting_items紀錄所有投票項目之名稱、ID
 @Getter
@@ -16,5 +22,11 @@ public class Voting_items {
 
     @Column
     private String name;
+
+
+    @OneToMany(targetEntity = Records.class, fetch = FetchType.EAGER)
+    @JoinColumn(name = "itemId", referencedColumnName = "id")
+    private Set<Records> records = new HashSet<>();
+
 
 }
